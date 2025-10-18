c3916f2b2539877c61020c0e2494a0f5c0d40d60
yanguadotdev <yanguasamir05@gmail.com>
2025-10-18 14:18:33 -0500

    add background to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,7 +26,18 @@ export default function RootLayout({
   return (
     <html lang='en'>
       <body
-        className={`${hostGrotesk.variable} ${geistMono.variable} antialiased`}
+        className={`${hostGrotesk.variable} ${geistMono.variable} antialiased bg-[#f8fafc]`}
+        style={{
+          backgroundImage: `
+        linear-gradient(to right, #e2e8f0 1px, transparent 1px),
+        linear-gradient(to bottom, #e2e8f0 1px, transparent 1px)
+      `,
+          backgroundSize: "20px 30px",
+          WebkitMaskImage:
+            "radial-gradient(ellipse 70% 60% at 50% 0%, #000 60%, transparent 100%)",
+          maskImage:
+            "radial-gradient(ellipse 70% 60% at 50% 0%, #000 60%, transparent 100%)",
+        }}
       >
         {children}
         <Toaster />
